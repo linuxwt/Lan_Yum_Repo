2fccc91295fc7b33e7d471ee8830df6656b076d5
linuxwt <31132875+linuxwt@users.noreply.github.com>
2019-06-25 17:24:36 +0800

    Update local.sh

--- local.sh
+++ local.sh
@@ -20,13 +20,16 @@ createrepo
 nginxrepo
 nginx
 
-mkdir -p /etc/yum.repo.d/bak
-mv CentOS* /etc/yum.repo.d/bak
+mkdir -p /etc/yum.repos.d/bak
+mv /etc/yum.repos.d/CentOS* /etc/yum.repos.d/bak
 
-cat <<EOF>> /etc/yum.repo.d/local.repo
+netcard=$(basename $(ls -la /etc/sysconfig/network-scripts/ifcfg* | grep -v lo | awk -F ' ' '{print $9}')) 
+Netcard=${netcard/ifcfg-/}
+ip=$(ip addr | grep ${netcard1} | grep inet | awk -F ' ' '{print $2}' | awk -F '/' '{print $1}')
+cat <<EOF>> /etc/yum.repos.d/local.repo
 [local]
 name=local
-baseurl=http://10.8.8.13/myshare
+baseurl=http://${ip}/myshare
 enabled=1
 gpgcheck=0
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
